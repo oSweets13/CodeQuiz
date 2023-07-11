f0ee534475bf14fe8877341545adb29cc31f8fa6
oSweets13 <olivia13perkins@gmail.com>
2023-07-10 19:33:36 -0600

    began testing some of the Javascript

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// Add all Variables on the top
+var beginQuiz = document.querySelector("#beginQuizButton")
+
+
+
+// Add Event Listener to Begin Quiz
+beginQuiz.addEventListener("click", (Event) => {
+    startScreen.textContent = "test"
+});
+
+//
